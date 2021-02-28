df2e84d81795ffca6f997f186fa50b3f405c64d0
mateuszjuszczel <!22Mati22!>
2021-02-28 14:43:31 +0100

    Return boolean if in board is empty space.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 def display_board(board):
     print(board[7] + '|' + board[8] + '|' + board[9])
     print(board[4] + '|' + board[5] + '|' + board[6])
@@ -19,37 +20,38 @@ def player_input():
 
 
 def place_marker(board, marker, position):
-
     board[position] = marker
 
 
 def win_check(board, mark):
-
     # Return True if 3 markers in a row is the same.
 
     # Win -
     return (
-        # Win -
-        (board[1] == board[2] == board[3] == mark) or
-        (board[4] == board[5] == board[6] == mark) or
-        (board[7] == board[8] == board[9] == mark) or
-        # Win |
-        (board[1] == board[4] == board[7] == mark) or
-        (board[2] == board[5] == board[8] == mark) or
-        (board[3] == board[6] == board[9] == mark) or
-        # Win /
-        (board[1] == board[5] == board[9] == mark) or
-        # Win \
-        (board[7] == board[5] == board[3] == mark)
-        )
+            # Win -
+            (board[1] == board[2] == board[3] == mark) or
+            (board[4] == board[5] == board[6] == mark) or
+            (board[7] == board[8] == board[9] == mark) or
+            # Win |
+            (board[1] == board[4] == board[7] == mark) or
+            (board[2] == board[5] == board[8] == mark) or
+            (board[3] == board[6] == board[9] == mark) or
+            # Win /
+            (board[1] == board[5] == board[9] == mark) or
+            # Win \
+            (board[7] == board[5] == board[3] == mark)
+    )
 
 
 def choose_first_palayer():
-
-    flip = random.randint(0,1)
+    flip = random.randint(0, 1)
 
     if flip == 0:
         return 'Player 1'
     else:
         return 'Player 2'
 
+
+def space_check(board, position):
+    # Return boolean if in board is empty space
+    return board[position] == ' '
