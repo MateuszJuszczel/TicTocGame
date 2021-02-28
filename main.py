93b485d5d3fa16aec270cce4c6337eabee21b444
mateuszjuszczel <!22Mati22!>
2021-02-28 14:36:21 +0100

    Check win, all possible variants.

--- main.py
+++ main.py
@@ -19,3 +19,24 @@ def player_input():
 def place_marker(board, marker, position):
 
     board[position] = marker
+
+
+def win_check(board, mark):
+
+    # Return True if 3 markers in a row is the same.
+
+    # Win -
+    return (
+        # Win -
+        (board[1] == board[2] == board[3] == mark) or
+        (board[4] == board[5] == board[6] == mark) or
+        (board[7] == board[8] == board[9] == mark) or
+        # Win |
+        (board[1] == board[4] == board[7] == mark) or
+        (board[2] == board[5] == board[8] == mark) or
+        (board[3] == board[6] == board[9] == mark) or
+        # Win /
+        (board[1] == board[5] == board[9] == mark) or
+        # Win \
+        (board[7] == board[5] == board[3] == mark)
+        )
