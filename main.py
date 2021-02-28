35132413ddb80c25ebff0468e56d00286cd83206
mateuszjuszczel <!22Mati22!>
2021-02-28 14:14:31 +0100

    Player input. Take the choose from player.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-
 def display_board(board):
-
     print(board[7] + '|' + board[8] + '|' + board[9])
     print(board[4] + '|' + board[5] + '|' + board[6])
     print(board[1] + '|' + board[2] + '|' + board[3])
+
+
+def player_input():
+    marker = ''
+
+    while marker != "X" and marker != "O":
+        marker = input('Player 1 : Choose X or O ').upper()
+
+    if marker == "X":
+        return "X", "O"
+    else:
+        return "O", "X"
